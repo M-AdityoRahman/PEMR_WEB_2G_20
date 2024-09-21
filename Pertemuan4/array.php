04bcda8dd39bbca8baec3a1ee4d21a7dd4e3caa4
M-AdityoRahman <adityorahman18@gmail.com>
2024-09-21 19:27:19 +0700

    Pertemuan4_Soal5.2

--- Pertemuan4/array.php
+++ Pertemuan4/array.php
@@ -10,3 +10,30 @@ foreach ($nilaiSiswa as $nilai) {
 }
 
 echo "Daftar nilai siswa yang lulus : " . implode(', ', $nilaiLulus);
+
+echo "<br><br>";
+
+$daftarKaryawan = [
+    ['Alice', 7],
+    ['Bob', 3],
+    ['Charlie', 9],
+    ['David', 5],
+    ['Eva', 6],
+];
+
+$karyawanPengalamanLimaTahun = [];
+
+foreach ($daftarKaryawan as $karyawan) {
+    if ($karyawan[1] > 5) {
+        $karyawanPengalamanLimaTahun[] = $karyawan[0];
+    }
+}
+
+echo "Daftar Karyawan dengan Pengalaman Kerja Lebih dari 5 Tahun: ";
+echo "<ul>";
+foreach ($karyawanPengalamanLimaTahun as $karyawan) {
+    echo "<li>$karyawan</li>";
+}
+echo "</ul>";
+
+?>
