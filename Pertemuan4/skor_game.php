3d65c1b62a9311a7527c284ff7446d112b1cf3b0
M-AdityoRahman <adityorahman18@gmail.com>
2024-09-21 18:52:46 +0700

    Pertemuan4_Soal4.8

---
+++ Pertemuan4/skor_game.php
@@ -0,0 +1,7 @@
+<?php
+$totalSkor = 550;
+$hadiahTambahan = ($totalSkor > 500) ? "YA" : "TIDAK";
+echo "<h3>Skor Pemain dan Hadiah Tambahan:</h3>";
+echo "Total skor pemain adalah: $totalSkor<br>";
+echo "Apakah pemain mendapatkan hadiah tambahan? $hadiahTambahan";
+?>
