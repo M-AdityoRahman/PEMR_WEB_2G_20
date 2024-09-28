8ed80c021c3dee493e89929c64e822b164de4f7a
M-AdityoRahman <adityorahman18@gmail.com>
2024-09-28 19:39:50 +0700

    Pertemuan5_PHP_Soal6

--- Pertemuan5/rekursif.php
+++ Pertemuan5/rekursif.php
@@ -1,9 +1,11 @@
 <?php
-function tampilkanHaloDunia(){
-    echo "Halo Dunia! <br>";
-
-    tampilkanHaloDunia();
+function tampilkanAngka (int $jumlah, int $indeks = 1) {
+    echo "Perulangan ke-{$indeks} <br>";
+    
+    //panggil diri sendiri selama $indeks <= $jumlah
+    if ($indeks < $jumlah) {
+        tampilkanAngka ($jumlah, $indeks + 1);
+    }
 }
-
-tampilkanHaloDunia();
+tampilkanAngka (20);
 ?>
