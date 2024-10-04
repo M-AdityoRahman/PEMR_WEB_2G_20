536e91229bae85bcd1e60c0cbaaac75151e6a87f
M-AdityoRahman <adityorahman18@gmail.com>
2024-10-04 08:54:13 +0700

    Pertemuan6_Soal5

--- Pertemuan6/praktik_php/fungsi.php
+++ Pertemuan6/praktik_php/fungsi.php
@@ -1,12 +1,19 @@
 <?php
 
-function perkenalan(){
-    echo "Assalamualaikum, ";
-    echo "Perkenalkan, nama saya Adit<br/>"; // tulis sesuai dengan nama kalian
+function perkenalan($nama, $salam){
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya " .$nama. "<br/>"; // tulis sesuai dengan nama kalian
     echo "Senang berkenalan dengan Anda<br/>";
 }
 
 //memanggil fungsi yang sudah dibuat
-perkenalan();
-perkenalan();
+perkenalan("Hamdana", "Hallo");
+
+echo "<hr>";
+
+$saya = "Elok";
+$ucapanSalam = "Selamat Pagi";
+
+//memanggil lagi
+perkenalan($saya, $ucapanSalam);
 ?>
