148df2c9036235b4c87b80aff2a61670e871a5ba
M-AdityoRahman <adityorahman18@gmail.com>
2024-10-04 08:20:31 +0700

    Pertemuan6_Soal1_perulangan

--- Pertemuan6/array_1.php
+++ Pertemuan6/array_1.php
@@ -9,9 +9,9 @@
     <?php
     $Listdosen = ["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
 
-    echo $Listdosen[2] . "<br>";
-    echo $Listdosen[0] . "<br>";
-    echo $Listdosen[1] . "<br>";
+    for ($i = 0; $i < count($Listdosen); $i++) {
+        echo $Listdosen[$i] . "<br>";
+    }
     ?>
 </body>
 
