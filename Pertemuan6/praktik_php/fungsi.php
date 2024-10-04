4b20c2f6f3a88e7d0e2d2bbb5a7e5811401c15ab
M-AdityoRahman <adityorahman18@gmail.com>
2024-10-04 08:41:33 +0700

    Pertemuan6_Soal4

---
+++ Pertemuan6/praktik_php/fungsi.php
@@ -0,0 +1,12 @@
+<?php
+
+function perkenalan(){
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Adit<br/>"; // tulis sesuai dengan nama kalian
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+//memanggil fungsi yang sudah dibuat
+perkenalan();
+perkenalan();
+?>
