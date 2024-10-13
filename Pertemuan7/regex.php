86eab1217bf87466e4b6dc701f75f2b5b26a2d96
M-AdityoRahman <adityorahman18@gmail.com>
2024-10-13 16:50:42 +0700

    Pertemuan7_Soal5.5

--- Pertemuan7/regex.php
+++ Pertemuan7/regex.php
@@ -35,3 +35,14 @@ if (preg_match($pattern, $text, $matches)) {
     echo "Tidak ada yang cocok!";
     echo "<br>";
 }
+
+$pattern = '/go?d/'; // cocokkan "god", "good"
+$text = 'god is good';
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: " . $matches[0];
+    echo "<br>";
+
+} else {
+    echo "Tidak ada yang cocok!";
+    echo "<br>";
+}
