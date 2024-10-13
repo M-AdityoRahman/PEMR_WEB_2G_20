0dbbbaf104c817fdc18071e88dbee4798434e44a
M-AdityoRahman <adityorahman18@gmail.com>
2024-10-13 18:23:13 +0700

    Pertemuan7_Soal7.2

--- Pertemuan7/form_validasi.php
+++ Pertemuan7/form_validasi.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
+<html lang="en">
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form Input dengan Validasi</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 
 <body>
@@ -9,13 +13,42 @@
     <form id="myForm" action="proses_validasi.php" method="post">
         <label for="nama">Nama:</label>
         <input type="text" id="nama" name="nama">
+        <span id="nama-error" style="color: red;"></span><br>
         <br>
 
         <label for="email">Email:</label>
         <input type="text" id="email" name="email">
+        <span id="email-error" style="color: red;"></span><br>
         <br>
+
         <input type="submit" value="Submit">
     </form>
+    <script>
+        $(document).ready(function () {
+            $("#myForm").submit(function (event) {
+                var nama = $("#nama").val();
+                var email = $("#email").val();
+                var valid = true;
+
+                if (nama == "") {
+                    $("#nama-error").text("Nama harus diisi!");
+                    valid = false;
+                } else {
+                    $("#nama-error").text("");
+                }
+                if (email === "") {
+                    $("#email-error").text("Email harus diisi!");
+                    valid = false;
+                } else {
+                    $("#email-error").text("");
+                }
+
+                if (!valid) {
+                    event.preventDefault();
+                }
+            })
+        })
+    </script>
 </body>
 
 </html>
