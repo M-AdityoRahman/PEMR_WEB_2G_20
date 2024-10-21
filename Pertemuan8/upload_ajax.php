55df67890ba1178b4d4374019013c34e4b85c329
M-AdityoRahman <adityorahman18@gmail.com>
2024-10-21 14:43:30 +0700

    Pertemuan8_Soal12

--- Pertemuan8/upload_ajax.php
+++ Pertemuan8/upload_ajax.php
@@ -1,32 +1,26 @@
 <?php
 if (isset($_FILES['file'])) {
     $errors = array();
-    $allowed_extensions = array("jpg", "png", "jpeg");
-    $file_count = count($_FILES['file']['name']); // Hitung jumlah file
+    $file_name = $_FILES['file']['name'];
+    $file_size = $_FILES['file']['size'];
+    $file_tmp = $_FILES['file']['tmp_name'];
+    $file_type = $_FILES['file']['type'];
+    @$file_ext = strtolower("" . end(explode('.', $_FILES['file']['name'])) . "");
+    $extensions = array("jpg", "png", "jpeg");
 
-    for ($i = 0; $i < $file_count; $i++) {
-        $file_name = $_FILES['file']['name'][$i];
-        $file_size = $_FILES['file']['size'][$i];
-        $file_tmp = $_FILES['file']['tmp_name'][$i];
-        $file_type = $_FILES['file']['type'][$i];
-        @$file_ext = strtolower(end(explode('.', $_FILES['file']['name'][$i])));
 
-        if (!in_array($file_ext, $allowed_extensions)) {
-            $errors[] = "$file_name: Ekstensi file yang diizinkan hanya jpg, png, dan jpeg.";
-        }
-
-        if ($file_size > 2097152) {
-            $errors[] = "$file_name: Ukuran file tidak boleh lebih dari 2 MB.";
-        }
-
-        if (empty($errors)) {
-            move_uploaded_file($file_tmp, "uploads/" . $file_name);
-            echo "$file_name berhasil diunggah.<br>";
-        }
+    if (in_array($file_ext, $extensions) === false) {
+        $errors[] = "Ekstensi file yang diizinkan adalah jpg, png dan jpeg.";
     }
 
-    if (!empty($errors)) {
-        echo implode("<br>", $errors);
+    if ($file_size > 2097152) {
+        $errors[] = 'Ukuran file tidak boleh lebih dari 2 MB';
+    }
+
+    if (empty($errors) == true) {
+        move_uploaded_file($file_tmp, "uploads/" . $file_name);
+        echo "File berhasil diunggah.";
+    } else {
+        echo implode(" ", $errors);
     }
 }
-?>
