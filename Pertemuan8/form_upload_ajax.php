7150efb3d2897cac24b0d74793d01f3291c18d70
M-AdityoRahman <adityorahman18@gmail.com>
2024-10-21 14:30:06 +0700

    Pertemuan8_Soal11

--- Pertemuan8/form_upload_ajax.php
+++ Pertemuan8/form_upload_ajax.php
@@ -7,13 +7,13 @@
     <div class="upload-form-container">
         <h2>Unggah File Dokumen</h2>
         <form id="upload-form" action="upload_ajax.php" method="post" enctype="multipart/form-data">
-            <input type="file" name="file" id="file">
+            <input type="file" name="file[]" id="file" multiple="multiple">
             <input type="submit" name="submit" value="Unggah">
         </form>
         <div id="status"></div>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" ></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="upload.js"></script>
 </body>
 </html>
--- Pertemuan8/upload_ajax.php
+++ Pertemuan8/upload_ajax.php
@@ -1,26 +1,32 @@
 <?php
 if (isset($_FILES['file'])) {
     $errors = array();
-    $file_name = $_FILES['file']['name'];
-    $file_size = $_FILES['file']['size'];
-    $file_tmp = $_FILES['file']['tmp_name'];
-    $file_type = $_FILES['file']['type'];
-    @$file_ext = strtolower("" . end(explode('.', $_FILES['file']['name'])) . "");
-    $extensions = array("pdf", "doc", "docx", "txt");
+    $allowed_extensions = array("jpg", "png", "jpeg");
+    $file_count = count($_FILES['file']['name']); // Hitung jumlah file
 
+    for ($i = 0; $i < $file_count; $i++) {
+        $file_name = $_FILES['file']['name'][$i];
+        $file_size = $_FILES['file']['size'][$i];
+        $file_tmp = $_FILES['file']['tmp_name'][$i];
+        $file_type = $_FILES['file']['type'][$i];
+        @$file_ext = strtolower(end(explode('.', $_FILES['file']['name'][$i])));
 
-    if (in_array($file_ext, $extensions) === false) {
-        $errors[] = "Ekstensi file yang diizinkan adalah PDF, DOC, DOCX, dan TXT.";
+        if (!in_array($file_ext, $allowed_extensions)) {
+            $errors[] = "$file_name: Ekstensi file yang diizinkan hanya jpg, png, dan jpeg.";
+        }
+
+        if ($file_size > 2097152) {
+            $errors[] = "$file_name: Ukuran file tidak boleh lebih dari 2 MB.";
+        }
+
+        if (empty($errors)) {
+            move_uploaded_file($file_tmp, "uploads/" . $file_name);
+            echo "$file_name berhasil diunggah.<br>";
+        }
     }
 
-    if ($file_size > 2097152) {
-        $errors[] = 'Ukuran file tidak boleh lebih dari 2 MB';
-    }
-
-    if (empty($errors) == true) {
-        move_uploaded_file($file_tmp, "uploads/" . $file_name);
-        echo "File berhasil diunggah.";
-    } else {
-        echo implode(" ", $errors);
+    if (!empty($errors)) {
+        echo implode("<br>", $errors);
     }
 }
+?>
